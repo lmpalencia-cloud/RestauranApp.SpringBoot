94ef6576683031a30d1754f0c714862853ef212a
SimonEspinosa85 <simonespinosaarteaga@gmail.com>
2025-11-30 13:44:39 -0500

    Creación del metodo CommandLineRunner con @Bean para creacion de mesa

--- restaurante/src/main/java/com/simonyluismario/restaurante/RestauranteApplication.java
+++ restaurante/src/main/java/com/simonyluismario/restaurante/RestauranteApplication.java
@@ -1,7 +1,12 @@
 package com.simonyluismario.restaurante;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.simonyluismario.restaurante.models.TableEntity;
+import com.simonyluismario.restaurante.repositories.TableRepository;
 
 @SpringBootApplication
 public class RestauranteApplication {
@@ -9,5 +14,19 @@ public class RestauranteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestauranteApplication.class, args);
 	}
+	@Bean
+public CommandLineRunner init(TableRepository repo){
+    return args -> {
+        if(repo.count() == 0){
+            for(int i=1; i<=20; i++){
+                TableEntity t = new TableEntity();
+                t.setName("Mesa " + i);
+                t.setOccupied(false);
+                repo.save(t);
+            }
+        }
+    };
+}
+
 
 }
