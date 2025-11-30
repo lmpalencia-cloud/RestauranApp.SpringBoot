a223c987866b6fde81853634a8e28ab091c7e828
SimonEspinosa85 <simonespinosaarteaga@gmail.com>
2025-11-30 13:58:16 -0500

    Se añadió un encontrar mejor vendedor

--- restaurante/src/main/java/com/simonyluismario/restaurante/repositories/OrderRepository.java
+++ restaurante/src/main/java/com/simonyluismario/restaurante/repositories/OrderRepository.java
@@ -2,6 +2,18 @@ package com.simonyluismario.restaurante.repositories;
 
 
 import com.simonyluismario.restaurante.models.*;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<OrderEntity, Long> {}
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+        @Query("SELECT o.worker.username, SUM(o.total) as totalVendio " +
+           "FROM OrderEntity o " +
+           "WHERE o.paid = true " +
+           "GROUP BY o.worker.username " +
+           "ORDER BY totalVendio DESC")
+        List<Object[]> findTopSellers();
+}
+
